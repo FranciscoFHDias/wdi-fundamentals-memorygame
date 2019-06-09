058392411797597c477da7b243ae2f74f18855fe
Francisco Dias <franciscodias@Franciscos-MacBook-Pro.local>
2019-06-09 19:34:07 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,21 +2,22 @@ const cards = ['queen', 'queen', 'king', 'king'];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log('User flipped ' + cardOne)
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped ' + cardTwo)
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-  alert ('You found a match!')
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log('You found a match!');
+  }
+    else {
+    console.log('Sorry, try again.');
+  }
 }
-  else {
-  alert ('Sorry, try again.');
+
+function flipCard(cardId) {
+  console.log('User flipped ' + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  if (cardsInPlay.length === 2){
+    checkForMatch ();
+  }
 }
+
+flipCard (0);
+flipCard (2);
