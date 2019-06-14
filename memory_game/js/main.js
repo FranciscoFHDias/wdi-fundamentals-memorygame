226db6e271a98101fadc808a24b6abf375da954e
Francisco Dias <franciscodias@Franciscos-MacBook-Pro.local>
2019-06-14 08:35:07 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,24 +23,37 @@ const cards = [
 
 var cardsInPlay = [];
 
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  };
+};
+
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert('You found a match!');
   }
     else {
     alert('Sorry, try again.');
-  }
-}
+  };
+};
 
-function flipCard(cardId) {
+function flipCard() {
+  var cardId = this.getAttribute('data-id');
+  var cardElement = this.setAttribute('src', cards[cardId].cardImage);
   console.log('User flipped ' + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
   if (cardsInPlay.length === 2){
-    checkForMatch ();
-  }
-}
+    checkForMatch();
+  };
+};
 
-flipCard (0);
-flipCard (2);
+
+
+createBoard();
